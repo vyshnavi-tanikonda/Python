aae40557990b42062ab1253d7f51ca2d848456fc
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2026-03-10 21:14:22 +0530

    Add files via upload

---
+++ Input_Output.py
@@ -0,0 +1,14 @@
+# Input and Output
+
+""" num = input("Give your number: ")  # Input
+print(type(num))  # gives output= string
+
+res = int(num)
+print(res)
+print(type(res))  # output  """
+
+# example
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hi,"+ name + " ! Your age is: ", age)
