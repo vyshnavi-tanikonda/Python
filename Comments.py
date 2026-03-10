1c6c573196c66c83b827f61ee2332f652f4cf383
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2026-03-10 21:16:26 +0530

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,7 @@
+# This is s single line comment
+
+
+""" it is a multi line comment , it is also called docstring
+we can write infinite lines in this multiline commenting"""
+
+''' it is also a multiline comment'''
